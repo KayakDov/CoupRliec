6de0a15fa5b6ec5f5bb55c23f516375aea643a4c
Dov Neimand <kayakdov@gmail.com>
2021-01-19 13:17:29 -0500

    not filtering affine spaces that have a projection function.

--- src/Convex/thesisProjectionIdeas/GradDescentFeasibility/LocalPolyhedralCone.java
+++ src/Convex/thesisProjectionIdeas/GradDescentFeasibility/LocalPolyhedralCone.java
@@ -101,7 +101,7 @@ public class LocalPolyhedralCone extends Polytope {
                     .filter(as -> aspb.projectionRule(as, preProj, epsilon))
                     .map(as -> new ASProj(as, preProj))
                     .filter(asProj -> hasElement(asProj.proj()))
-                    .findAny()
+                    .min(Comparator.comparing(p -> p.proj().d(preProj)))
                     .orElse(null);
 
             if (tryTravelThrough != null) return tryTravelThrough;
